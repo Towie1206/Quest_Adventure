94991d427178588ce5df82482bb348c733a9ad7c
Nguyen Cong Bang <ncongbang2006@gmail.com>
2026-06-08 01:19:43 +0700

    move and wallslide upgrade

--- Assets/Script/Player_IdleState.cs
+++ Assets/Script/Player_IdleState.cs
@@ -16,6 +16,9 @@ public class Player_IdleState : Player_GroundedState
     public override void Update()
     {
         base.Update();
+        //if player try to move facing direction annd direction detected wall, so still in idle
+        if (player.moveInput.x == player.facingDir && player.wallDetected)
+            return;
         if (player.moveInput.x != 0)
         {
             stateMachine.ChangeState(player.moveState);
